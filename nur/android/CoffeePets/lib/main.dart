a20f854d916c29b748a2bb76665dc1c1368404ae
Sara <saritaprieto445@gmail.com>
2023-05-22 02:05:12 -0500

    todo lo que va del trimestre 6

---
+++ nur/android/CoffeePets/lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:coffeepets/screen/splash.dart';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+<<<<<<< HEAD
+=======
+      //theme: ThemeData(fontFamily: 'Raleway'),
+>>>>>>> 1db948d (proyecto flutter adso6)
+      home:splash(),
+    );
+  }
+}
